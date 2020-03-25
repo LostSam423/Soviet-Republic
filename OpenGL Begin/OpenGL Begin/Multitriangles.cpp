2fc5ab3ef2a3b592a9a3f2f806ba1ea59b1039f5
LostSam423 <beherasambit423@gmail.com>
2020-03-25 12:20:08 +0530

    F with extra steps

--- OpenGL Begin/OpenGL Begin/Multitriangles.cpp
+++ OpenGL Begin/OpenGL Begin/Multitriangles.cpp
@@ -9,8 +9,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const unsigned int SCR_WIDTH = 1080;
+const unsigned int SCR_HEIGHT = 1080;
 const GLchar *vertexShaderSource ="#version 330 core\n"
 "layout ( location = 0 ) in vec3 position;\n"
 "void main ( )\n "
@@ -21,7 +21,7 @@ const GLchar *fragmentShaderSource ="#version 330 core\n"
 "out vec4 color;\n"
 "void main ( )\n "
 "{\n"
-"color = vec4( 1.0f, 0.5f, 0.2f, 1.0f );\n"
+"color = vec4( 1.0f, 0.0f, 0.0f, 1.0f );\n"
 "}";
 int main()
 {
@@ -35,7 +35,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "tu as de beaux grands yeux!!!", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -85,19 +85,41 @@ int main()
     
     GLfloat vertices[] =
     {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f
+        -0.2f, 0.6f,  0.0f,
+        -0.2f, 0.4f, 0.0f,
+        -0.2f, -0.5f, 0.0f,
+        0.0f, -0.5f, 0.0f,  //3
+        0.0f, 0.0f , 0.0f,  //4
+        0.4f, 0.0f, 0.0f,   //5
+        0.4f, 0.2f, 0.0f,//6
+        0.0f,0.2f,0.0f,//7
+        0.0f, 0.4f, 0.0f,//8
+        0.6f, 0.4f, 0.0f,//9
+        0.6f,0.6f,0.0f
+
+    };
+    GLuint indices[] =
+    {
+        0, 1, 10,
+        10, 9, 1,
+        1, 2, 3,
+        1, 3, 8,
+        4, 5, 6,
+        6, 4, 7,
     };
     
-    GLuint VBO, VAO;
+    GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers( 1, &VBO );
+    glGenBuffers( 1, &EBO);
     
     glBindVertexArray( VAO );
     
     glBindBuffer( GL_ARRAY_BUFFER, VBO );
     glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
+    
+    glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
+    glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3* sizeof( GLfloat ), (GLvoid * ) 0 );
     glEnableVertexAttribArray( 0 );
@@ -112,11 +134,11 @@ int main()
 
         // render
         // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram( shaderProgram );
         glBindVertexArray( VAO );
-        glDrawArrays( GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, 0);
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
@@ -124,6 +146,7 @@ int main()
     }
     glDeleteVertexArrays( 1, &VAO );
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
     glfwTerminate();
